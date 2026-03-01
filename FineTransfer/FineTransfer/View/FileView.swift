6258dec7550fab918f2586d9b52ba5a4b201262e
Tzu-Yi Lin <ehrippura@gmail.com>
2026-03-01 11:08:21 +0800

    Add folder navigation with back/forward history to FileView

--- FineTransfer/FineTransfer/View/FileView.swift
+++ FineTransfer/FineTransfer/View/FileView.swift
@@ -12,18 +12,68 @@ struct FileView: View {
     var device: MTPDevice?
 
     @State private var files: [MTPFileItem] = []
+    @State private var currentFolderID: UInt32 = MTPDevice.rootFolderID
+    @State private var backStack: [UInt32] = []
+    @State private var forwardStack: [UInt32] = []
 
     var body: some View {
         FileGridView(files: files)
             .onDoubleClick { item in
-                print("double click item: \(item)")
+                if item.isFolder {
+                    navigateToFolder(item.itemID)
+                }
             }
             .onAppear {
                 loadFiles()
             }
             .onChange(of: device) {
+                resetNavigation()
                 loadFiles()
             }
+            .toolbar {
+                ToolbarItemGroup(placement: .navigation) {
+                    Button(action: goBack) {
+                        Image(systemName: "chevron.left")
+                    }
+                    .disabled(backStack.isEmpty)
+
+                    Button(action: goForward) {
+                        Image(systemName: "chevron.right")
+                    }
+                    .disabled(forwardStack.isEmpty)
+                }
+            }
+    }
+
+    private func navigateToFolder(_ folderID: UInt32) {
+        backStack.append(currentFolderID)
+        forwardStack.removeAll()
+        currentFolderID = folderID
+        loadFiles()
+    }
+
+    private func goBack() {
+        guard let previousFolderID = backStack.popLast() else {
+            return
+        }
+        forwardStack.append(currentFolderID)
+        currentFolderID = previousFolderID
+        loadFiles()
+    }
+
+    private func goForward() {
+        guard let nextFolderID = forwardStack.popLast() else {
+            return
+        }
+        backStack.append(currentFolderID)
+        currentFolderID = nextFolderID
+        loadFiles()
+    }
+
+    private func resetNavigation() {
+        currentFolderID = MTPDevice.rootFolderID
+        backStack.removeAll()
+        forwardStack.removeAll()
     }
 
     private func loadFiles() {
@@ -31,7 +81,7 @@ struct FileView: View {
             files = []
             return
         }
-        files = (try? device.contents(folderID: MTPDevice.rootFolderID, storageID: device.rootStorageID)) ?? []
+        files = (try? device.contents(folderID: currentFolderID, storageID: device.rootStorageID)) ?? []
     }
 }
 
